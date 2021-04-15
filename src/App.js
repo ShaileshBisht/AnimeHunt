873f7bda0edb3032e4217fc91f2e982ddade0937
shaileh Bisht <shaileshbisht765@gmail.com>
2021-04-16 00:14:55 +0530

    create NavBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <div className="app">
-      <h1>welcome</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/component/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navBar">
+      <h1>i am NAVBAR</h1>
+    </div>
+  );
+}
+
+export default NavBar;
