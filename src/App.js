419bba2fef00df7cc8ac9eebcc3811bfe1c364bf
shaileh Bisht <shaileshbisht765@gmail.com>
2021-04-16 01:00:34 +0530

    create animeDisplay component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import AnimeDisplay from "./component/AnimeDisplay";
 import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <AnimeDisplay />
     </div>
   );
 }
---
+++ src/component/AnimeDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AnimeDisplay.css";
+
+function AnimeDisplay() {
+  return (
+    <div className="animeDisplay">
+      <h1>i am anime Display</h1>
+    </div>
+  );
+}
+
+export default AnimeDisplay;
