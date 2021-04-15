9a1cc3829c3eb2923169cbc71217c0a345e2e68c
shaileh Bisht <shaileshbisht765@gmail.com>
2021-04-16 03:25:59 +0530

    add routes to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./App.css";
 import AnimeDisplay from "./component/AnimeDisplay";
 import NavBar from "./component/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <NavBar />
-      <AnimeDisplay />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <NavBar />
+            <h1>search</h1>
+          </Route>
+          <Route exact path="/">
+            <AnimeDisplay />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
