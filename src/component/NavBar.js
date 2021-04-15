ce4a672f5eb2825f4d1cc4a2b32f2b9dd9a4e805
shaileh Bisht <shaileshbisht765@gmail.com>
2021-04-16 00:20:17 +0530

    add content in NavBar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,7 +4,19 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <div className="navBar">
-      <h1>i am NAVBAR</h1>
+      <div className="navBar__box container">
+        <div className="navBar__left">
+          <div className="navBar__logo">
+            <p>animeKING</p>
+          </div>
+        </div>
+        <div className="navBar__right">
+          <div className="navBar__btn">
+            <button className="btn">Search</button>
+            <button className="btn">Sign In</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
